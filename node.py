3d3a1f8da6a16085b893bb1025f6e7d77dbe33ec
eug <eugypalu@gmail.com>
2019-04-27 11:58:31 +0200

    node, coordinator and util

--- node.py
+++ node.py
@@ -29,8 +29,10 @@ class node:
     def __createId(self, ip, port):
         return hash(ip + port)
     
+    
     def getId(self):
         return self._nodeId
+
 
     def insertNode(self, id):
         timestamp = time.time()
@@ -53,50 +55,68 @@ class node:
                 self._routingTable[h][i]['time'] = timestamp
 
 
-    def findNode(self, id, k):
-        #secondo me deve scorrere tutta la routing table per poi restituire i k più vicini
+    def findNode(self, id, dim):
         closestNode = []
-        for rt in self._routingTable:
-            for buck in rt:
-                self.distance(id, buck['id'])
-                #inserisco finchè non ho raggiunto k
-                #ordino la lista 
-                #sostituisco gli elementi presenti con quelli aventi distanza minore
-                #in fine restituisco una lista dei k dizionari (nodi) più vicini al nodo passato alla findNode
-        #potrei anche controllare nel bucket "giusto" considerando l'id e l'altezza della kbucket poi muovermi +1/-1 rispetto a quella.
-        #dovrebbe abbassarsi la complessità
-
-    def findNodeOptimized(self, id, k):
-        closestNode = []
-        temp = []
+        temp = None
         h = int(math.log2(id))
         k = h
-        while len(closestNode) < k and closestNode[k]['id'] != temp[k]['id']:
-            for i in range(self._bucketLength):
-                rtId = self._routingTable[k][i]['id']
-                distance = self.distance(id, rtId)
-                temp.append({'id': rtId, 'dist': distance})
-            #sort
-            #newlist = sorted(list_to_be_sorted, key=lambda k: k['name']) 
-            if k <= h:
-                k = k * -1 +1 
-            else:
-                k = k * -1 -1
+        move = 0 #mi serve per spostarmi su e giu nella rt
+        counterRt = 0
+        #finchè closestnode non è pieno, se è pieno controlla che non vengano aggiornati gli elementi
+        # (più vicini vengono sostituiti a più lontani)
+        #oppure finchè il numerro di 
+        while len(closestNode) < dim and closestNode[k]['id'] != temp or counterRt < self._numBit:
+            #uso una variabile "temporanea" per tenere l'ultimo elemento in memoria, se qualcosa cambia deve per forza cambiare anche 
+            #l'ultimo, dato che ordinando la lista l'ultimo è il più distante
+            temp = closestNode[k]['id']
+            for i in range(self._bucketLength):#conosco il numero di elementi della bucket quindi scorro sugli elementi
+                rtId = self._routingTable[k][i]['id']#prendo l'id in posizione i
+                distance = self.distance(id, rtId)#prendo la distanza in posizione i
+                if len(closestNode)  < dim:#se la lunghezza di closestnode è minore della lunghezza che deve essere restituita
+                    closestNode.append({'id': rtId, 'dist': distance})#aggiungo il nodo (id distanza) alla lista
+                else:#se invece la lista è piena
+                    #ordino la lista per la distanza in modo da avere l'ultimo elemento come il più distante
+                    closestNode = sorted(closestNode, key=lambda k: k['dist']) 
+                    if closestNode[k]['dist'] > distance: #se l'ultimo elemento di closestnode è maggiore di quello attuale allora li scambio
+                        #in quanto sto inserendo un nodo più vicino
+                        closestNode[k]['id'] = rtId
+                        closestNode[k]['dist'] = distance
+            
+            #ordino la lista in modo che se ci sono stati cambiamenti viene aggiornato l'ultimo elemento
+            closestNode = sorted(closestNode, key=lambda k: k['dist']) 
 
+            #questo serve per non scorrere tutta la bucketlist
+            #parto dall'altezza h per poi controllare quello immediatamente sotto e sopra, per coi continuare sopra e sotto
+            #finchè non riempo l'array o non vengono 
+            if k <= h:
+                move = move * -1 +1
+                k += move
+                #TODO aggiungere che se va oltre la lista devo decrementare l'indice dalla parte opposta
+            else:
+                move = move * -1 -1
+                k += move
+                #TODO aggiungere che se va oltre la lista devo decrementare l'indice dalla parte opposta
+
+            counterRt += 1
+        return closestNode
 
 
     def distance(self, node1, node2):
         return node1 ^ node2
-        
+
+
     def store(self, key, value):
         self._storedValue[key] = value 
         print('pair ' + key + ' '+ value + ' inserted')
 
+
     def findValue(self, key):
         return self._storedValue[key]
-    
+
+
     def getNodeState(self):
         return self._active
+
 
     def changeState(self):
         self._active = not self._active
